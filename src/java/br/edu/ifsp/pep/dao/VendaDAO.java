93d7a538a994dfed0b1b0d85c9b7f2a21814c56a
aluno <aluno@LAB06M29>
2022-05-20 15:57:37 -0300

    First

---
+++ src/java/br/edu/ifsp/pep/dao/VendaDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.dao;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author aluno
+ */
+@Stateless
+public class VendaDAO  extends AbstractDAO{
+    
+}
