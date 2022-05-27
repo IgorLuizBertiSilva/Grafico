546184933e7cc888c5968dc7ee98517e01dce07e
aluno <aluno@LAB06M37>
2022-05-27 16:20:28 -0300

    DTO Venda

---
+++ src/java/br/edu/ifsp/pep/dto/VentaDTO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.dto;
+
+/**
+ *
+ * @author aluno
+ */
+public class VentaDTO {
+    
+    private int mes;
+    
+    private double valor;
+
+    public int getMes() {
+        return mes;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    
+    
+    
+}
